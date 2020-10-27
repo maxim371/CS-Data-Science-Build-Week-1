7e0bcd7d1b8ce8fab6aae3df5461575328925151
maxim371 <kwaku.dr@gmail.com>
2020-10-26 23:53:27 -0400

    KNN from scratch with testing

--- KNN/knn.py
+++ KNN/knn.py
@@ -1,6 +1,6 @@
 import math
 import numpy as np
-import scipy
+from scipy import stats
 from sklearn import datasets
 from pandas import DataFrame
 
@@ -18,36 +18,41 @@ class K_Nearest_Neighbor:
     def __init__(self, k):
         self.k = k
 
-    def fit(self, x_train, y_train):
-        self.x_train = x_train
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
         self.y_train = y_train
 
-    def predict(self, x_test, target):
-        distances = self.distance(x_test)
+    def predict(self, X_test):
+        distances = self.get_distance(X_test)
         return self.labels(distances)
 
-    def euclidean_distance(self, vectorA, vectorB, length):
+    def euclidean_distance(self, point1, point2, length):
 
         distance = 0
         for x in range(length):
             distance += (point1[x] - point2[x])**2
         return np.sqrt(distance)
 
-    def distance(self, x_test):
+    def get_distance(self, X_test):
 
-        length = x_test.shape[1]
-        distance = []
-        for i in range(len(x_test)):
-            for row in range(len(self.x_train)):
-                dist = self.euclidean_distance(self.x_train.iloc[row], x_test.iloc[i], length)
-                distances.append(dist)
+        length = X_test.shape[1]                          
+        distances = []
+        for i in range(len(X_test)):
+            distances.append([X_test[i], [] ])
+            for a in self.X_train:
+                dist = self.euclidean_distance(a, X_test[i], length)
+                distances[i][1].append(dist)
 
         return distances
 
     def labels(self, distances):
 
-        y_indices = np.argsort(distances)[:self.k]
-        k_nearest_class = [self.y_train[i] for i in y_indices]
-        y_pred = [scipy.stats.mode(k_nearest_class)][0][0][0]
+        y_pred = []
+        for i in range(len(distances)):
+            distant = distances[i]
+            y_indices = np.argsort(distant[1])[:self.k]
+            k_nearest_class = [self.y_train[i%len(self.y_train)] for i in y_indices]
+            label = [stats.mode(k_nearest_class)][0][0][0]
+            y_pred.append(label)
         return y_pred
 
--- KNN/knn_test.py
+++ KNN/knn_test.py
@@ -1,19 +1,91 @@
-from sklearn import datasets
-from sklearn import metrics
-from sklearn.neighbors import KNeighborsClassifier
-import  numpy as np
 
-iris = datasets.load_iris()
-x = iris.data
+from knn import K_Nearest_Neighbor
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import scale
+from sklearn.cluster import KMeans 
+from sklearn.metrics import accuracy_score
+
+# Load Data
+iris = load_iris()
+X = scale(iris.data)
 y = iris.target
 
-k = 3
-model = KNeighborsClassifier(n_neighbors = k)
-model.fit(x, y)
+# Train/Test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=1)
 
-#Predictions
-print('(-3, -3, 3, 3) is class:')
-print(model.predict(x, [-3, -3, 3, 3]))
+# Choose K 
+clf = KNeighborsClassifier(n_neighbors=10)
 
-print('(2, 4, 5, 5, 4, 2) is class:')
-print(model.predict(x, [2, 4, 5, 5, 4, 2]))
+# Fit
+clf.fit(X_train, y_train)
+
+# Prediction
+predict = clf.predict(X_test)
+print("-------SKLEARN MODEL-------")
+print("Prediction", predict)
+
+# Accuracy Score
+print(f"Scikit-learn accuracy: {accuracy_score(y_test, predict)}")
+
+# y_pred
+y_pred = clf.predict([X_test[0]])
+print("y_pred", y_pred)
+
+########################################################################################
+
+# K_Nearest_Neighbor Model
+np_clf = K_Nearest_Neighbor(k=10)
+
+np_clf.fit(X_train, y_train)
+
+predict = np_clf.predict(X_test)
+print("-------PYTHON MODEL-------")
+print("Prediction", predict)
+
+# Accuracy Score
+print(f"K_Nearest_Neighbor accuracy: {accuracy_score(y_test, predict)}")
+
+##########################################################################
+
+classes = {0:'setosa', 1:'versicolor', 2:'virginica'}
+
+# Prediction on new data
+
+new_data = np.array([
+    [6, 4, 3, 2],
+    [10, 6, 8, 2]])
+
+new_data2 = np.array([
+    [5.1, 4.3, 3.2, 2.5],
+    [5, 0.9, 3.4, 1.0]])
+
+# Sklearn
+y_predict1 = clf.predict(new_data)
+print("-------NEW DATA-------")
+print(f"data 1:{classes[y_predict1[0]]}")
+print(f"data 2:{classes[y_predict1[1]]}")
+
+
+# K_Nearest_Neighbor
+y_predict2 = np_clf.predict(new_data)
+
+print(f"data 1:{classes[y_predict2[0]]}")
+print(f"data 2:{classes[y_predict2[1]]}")
+
+
+# Sklearn
+y_predict3 = clf.predict(new_data2)
+print("-------NEW DATA-------")
+print(f"data 1:{classes[y_predict3[0]]}")
+print(f"data 2:{classes[y_predict3[1]]}")
+
+
+# K_Nearest_Neighbor
+y_predict4 = np_clf.predict(new_data2)
+
+print(f"data 1:{classes[y_predict4[0]]}")
+print(f"data 2:{classes[y_predict4[1]]}")
